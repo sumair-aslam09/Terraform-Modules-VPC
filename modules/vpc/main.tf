403c81cdb38f162ec1b87f8e99b72d049fbbbb2a
MOHAMMAD SUMAIR <91409244+sumair-aslam09@users.noreply.github.com>
2023-05-12 12:19:35 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,6 @@
 #1.Create VPC
 resource "aws_vpc" "my-vpc" {
-    cidr_block=var.cidr
+    cidr_block=var.vpc_cidr
     instance_tenancy="default"
     enable_dns_support="true"
     enable_dns_hostnames="true"
